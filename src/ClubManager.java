905106eb3dcbadad5bc0912b27e613a608fa5805
hsk0316 <hsk63125645@gmail.com>
2024-12-01 22:44:45 +0900

    동아리 관리 프로그램 v0.1.0

---
+++ src/ClubManager.java
@@ -0,0 +1,21 @@
+/**
+ * 동아리 관리 프로그램
+ *
+ * <p>
+ * 이 프로그램은 동아리 등록, 조회, 활동 보고서 관리 등의 기능을 제공합니다.
+ * </p>
+ *
+ * @author 한승규
+ * @version --
+ * @since 2024-12-01
+ *
+ * @created 2024-12-01
+ */
+public class ClubManager {
+    /**
+    * 프로그램의 진입점 메소드입니다.
+    */
+ public static void main(String[] args) {
+        System.out.println("동아리 관리 프로그램");
+    }
+}
